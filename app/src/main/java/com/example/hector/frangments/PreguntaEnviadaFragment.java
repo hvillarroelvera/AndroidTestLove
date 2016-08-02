49fd5580b64d0740ddf676a5cc4c2fb865b08226
hvillarroelvera <hvillarroelvera@gmail.com>
2016-08-02 18:32:02 -0400

    Se agrega recyclerView a fragment PreguntaEnviada
    Logica incompleta

--- app/src/main/java/com/example/hector/frangments/PreguntaEnviadaFragment.java
+++ app/src/main/java/com/example/hector/frangments/PreguntaEnviadaFragment.java
@@ -18,33 +18,16 @@ import com.example.hector.testloveapp.R;
  * Use the {@link PreguntaEnviadaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PreguntaEnviadaFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class PreguntaEnviadaFragment extends Fragment implements RecyclerViewClickListener{
+    
     private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PreguntaEnviadaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    private RecyclerView recView;
+	private ArrayList<PreguntaDTO> datos = new ArrayList<PreguntaDTO>();
+   
+    
     public static PreguntaEnviadaFragment newInstance(String param1, String param2) {
         PreguntaEnviadaFragment fragment = new PreguntaEnviadaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        
         return fragment;
     }
 
@@ -55,17 +38,22 @@ public class PreguntaEnviadaFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+       
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pregunta_enviada, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_pregunta_enviada, container, false);
+		setData();
+		//Inicialización RecyclerView
+        recView = (RecyclerView) rootView.findViewById(R.id.RecViewSolEnviadaPendiente);
+        recView.setHasFixedSize(true);
+		recView.setAdapter(adaptador);
+        recView.setLayoutManager(
+                new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+		return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,6 +93,14 @@ public class PreguntaEnviadaFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+	
+	public void setData(){
+        Bundle bundle  = this.getArguments();
+        if(bundle != null){
+            datos = bundle.getParcelableArrayList(Constantes.PARCEL_LISTA_PREG_ENVIADA);
+        }
+
     }
 
 }
